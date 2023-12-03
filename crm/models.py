133c42f23129aebce91a7963c02abca13e0cbca2
Jason Ling <jasonling1996@gmail.com>
2023-12-03 08:31:00 -0800

    Create db model

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Renter(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.CharField(max_length=100)
+  telephone = models.CharField(max_length= 10)
+  gender = models.CharField(max_length=15)
