c587df8c0fcd15290ce3d90dca743492b6362e8d
Jason Ling <jasonling1996@gmail.com>
2023-12-02 23:50:44 -0800

    Create login page w bootstrap

--- crm/urls.py
+++ crm/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('login/', views.verify_login, name='login'),
+    # path('logout/', views.verify_logout, name='logout'),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html', {})
+
+  if request.method == 'POST':
+    email = request.POST['email']
+    password = request.POST['password']
+
+    user = authenticate(request, username=email, password=password)
+    if user is not None:
+      login(request, user)
+      messages.success(request, "Logged In")
+      return redirect('home')
+    
+    else:
+      messages.success(request, "Please Try Again")
+      return redirect('home')
+
+  else:
+    return render(request, 'home.html', {})
+
+# def verify_login(request):
+#   pass
+
+# def verify_logout(request):
+#   pass
